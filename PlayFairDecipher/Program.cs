41a9ad8d421a650144237e4e40df274a89317d00
Lucas Begnini <lucasbegnini@gmail.com>
2018-05-12 11:40:07 -0300

    Create base project

---
+++ PlayFairDecipher/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlayFairDecipher.src;
+
+namespace PlayFairDecipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayFair _playFair = new PlayFair();
+            string text = "O mundo e bao sebastiao";
+            string cipherText = "TgzurtpfrqupdbtuaksV";
+            Console.WriteLine(text);
+            string compare = "mundo";
+            string[] testes = { "cipher", "gord", "car" };
+            List<string> results = new List<string>();
+
+            Parallel.ForEach(
+            testes,
+            (currentWord) =>
+            {
+                string plainText = _playFair.Decipher(cipherText, currentWord);
+                if (plainText.Contains(compare)) {
+                   results.Add(currentWord); 
+                }
+            }
+            );
+
+            if (results.Count == 0) {
+                Console.WriteLine("Nao existe palavras chaves que possa quebrar no dicionario");
+            } else
+            {
+                Console.WriteLine("as possiveis palavras sao:");
+                for(int i = 0; i < results.Count; i++)
+                {
+                    Console.WriteLine(results[i]);
+                }
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
